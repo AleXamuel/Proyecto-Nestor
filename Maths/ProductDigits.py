f11face629d186d8840a4773253d633f9275898a
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2024-06-13 14:51:32 -0500

    ProductDigits.py
    Math

--- Maths/ProductDigits.py
+++ Maths/ProductDigits.py
@@ -5,47 +5,16 @@
 import sys 
 def digitosdivisores(N):
     res = []
-    if N == 0:
-        res.append(0)
-        return res
-    if N == 1:
-        res.append(1)
-        return res
+    lista = reversed([i for i in range(2,10)])
     while(N != 1):
         num = N
-        
-        if N%9 == 0:
-            while(N%9) == 0:
-                N = N//9
-                res.append(9)        
-        if N%8 == 0:
-            while(N%8) == 0:
-                N = N//8
-                res.append(8)
-        if N%6 == 0:
-            while(N%6) == 0:
-                N = N//6
-                res.append(6)
-        if N%4 == 0:
-            while(N%4) == 0:
-                N = N//4
-                res.append(4)
-        if N%2 == 0:
-            while(N%2) == 0:
-                N = N//2
-                res.append(2)
-        if N%3 == 0:
-            while(N%3) == 0:
-                N = N//3
-                res.append(3)
-        if N%5 == 0:
-            N = N//5
-            res.append(5)
-        if N%7 == 0:
-            N = N//7
-            res.append(7)
-        elif N == num: 
-        # Se crea la variable num y se guarda el N del comienzo, para ver si ha surgido un cambio en N, si despues de pasar todos no ha cambiado, significa que sus divisores son mas grandes que 9
+        for i in lista:
+            if N%i==0:
+                while N%i==0:
+                    N//=i
+                    res.append(i)
+        if N==num:
+# Se crea la variable num y se guarda el N del comienzo, para ver si ha surgido un cambio en N, si despues de pasar todos no ha cambiado, significa que sus divisores son mas grandes que 9
             res.append(-1)
             return res
     return res
@@ -53,6 +22,12 @@ def digitosdivisores(N):
 n = int(sys.stdin.readline())
 for i in range(n):
     N = int(sys.stdin.readline())
+    if N==0:
+        print(0)
+        continue
+    if N==1:
+        print(1)
+        continue
     res = digitosdivisores(N)
     res.sort()
     if res[0] == -1:
