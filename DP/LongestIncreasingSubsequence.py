e7fa693db5f69d4bc9bf4e551cc679084aa19947
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2024-06-28 01:05:58 -0500

    LongestIncreasingSubsequence.py
    BruteForce

---
+++ DP/LongestIncreasingSubsequence.py
@@ -0,0 +1,12 @@
+#Solution time O(n^2)
+def f(A):
+    n = len(A)
+    lis = [1]*n
+    for k in range(n):
+        for i in range(k):
+            if A[i]<A[k]:
+                lis[k] = max(lis[k],lis[i]+1)
+    return max(lis)
+n = int(input())
+arr = [int(i) for i in input().split()]
+print(f(arr))
