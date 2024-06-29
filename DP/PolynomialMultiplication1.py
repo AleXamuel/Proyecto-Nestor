c418b9f57e29a0506ca42268c6fefb8be6fdfff8
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2024-06-29 17:54:05 -0500

    Create PolynomialMultiplication1.py

---
+++ DP/PolynomialMultiplication1.py
@@ -0,0 +1,20 @@
+def polyMultiplication(poli1,poli2):
+    m = len(poli1)
+    n = len(poli2)
+    sz = m+n-1
+    ans = [0]*sz
+    for i in range(m):
+        for j in range(n):
+            ans[i+j]+=poli1[i]*poli2[j]
+    return ans,sz
+t = int(input())
+for _ in range(t):
+    n1 = int(input())
+    poly1 = [int(i) for i in input().split()]
+    n2 = int(input())
+    poly2 = [int(i) for i in input().split()]
+    poly3,grado = polyMultiplication(poly1,poly2)
+    print(grado-1)
+    print(*poly3)
+
+
