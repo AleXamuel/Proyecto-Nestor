8c22d8cf215a124b6771b8b11172a62283ab76b9
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2024-08-03 19:38:12 -0500

    Create Template Fast Input Java.java
    Input class

---
+++ Template Fast Input Java.java
@@ -0,0 +1,55 @@
+static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream f) {
+			br = new BufferedReader(new InputStreamReader(f));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public String nextLine() throws Exception {
+			return br.readLine();
+		}
+
+		public void close() throws IOException {
+			br.close();
+		}
+
+		public boolean hasNext() {
+			try {
+				while (st == null || !st.hasMoreTokens())
+					st = new StringTokenizer(br.readLine());
+
+			} catch (Exception e) {
+				return false;
+			}
+			return true;
+		}
+
+		public boolean ready() throws IOException {
+			return br.ready();
+		}
+
+	}
