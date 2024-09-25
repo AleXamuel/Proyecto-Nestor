600801b2be07b9fb4165afb4b535830389617fb1
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2024-09-25 13:51:33 -0500

    Update Power Strings.java

--- Strings/Power Strings.java
+++ Strings/Power Strings.java
@@ -1,41 +1,84 @@
 // https://vjudge.net/problem/UVA-10298#google_vignette
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Objects;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        while(true){
-            String s=bf.readLine();
-            if(Objects.equals(s,"."))
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner();
+        PrintWriter pw = new PrintWriter(System.out);
+        while (true){
+            String r=sc.next();
+            if(r.equals("."))
                 break;
-            LinkedList<Integer> divisores=new LinkedList<>();
-            for (int i = 1; i <= s.length()/2; i++)
-                if(s.length()%i==0)
-                    divisores.add(i);
-            int size=1;
-            for(int i: divisores){
-                if(size!=1)
-                    break;
-                boolean cambio= true;
-                String p1=s.substring(0,i);
-                for (int j = i; j+i <=s.length() ; j+=i) {
-                    if(!cambio)
+            LinkedList<Integer> div=new LinkedList<>();
+            for (int i = 1; i <=r.length()/2 ; i++)
+                if(r.length()%i==0)
+                    div.add(i);
+            boolean flag=true;
+            int ans=-1;
+            for(int i : div){
+                String s = r.substring(0,i);
+                flag=true;
+                for (int j = i; j <= r.length()-i; j+=i) {
+                    String t=r.substring(j,j+i);
+                    if(!s.equals(t)){
+                        flag=false;
                         break;
-                    String p2=s.substring(j,j+i);
-                    if(!p1.equals(p2))
-                        cambio=false;
+                    }
                 }
-                if(cambio)
-                    size = s.length()/i;
-
-
+                if(flag){
+                    ans=i;
+                    break;
+                }
             }
-            System.out.println(size);
+            if(ans<1)
+                System.out.println(1);
+            else
+                System.out.println(r.length()/ans);
 
         }
+
+
+    }
+    static class Scanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public Scanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        char nextChar() throws IOException {
+            return (char) br.read();
+        }
+
     }
 }
