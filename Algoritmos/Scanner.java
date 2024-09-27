d742fe56b1fdbbdb6aa7c3d9d2f7a0e8f8de615b
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2024-09-26 21:40:56 -0500

    Create Scanner.java

---
+++ Algoritmos/Scanner.java
@@ -0,0 +1,58 @@
+static class Scanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Scanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		char nextChar() throws IOException {
+            		return (char) br.read(); 
+        	}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public String nextLine() throws Exception {
+			return br.readLine();
+		}
+
+		public void close() throws IOException {
+			br.close();
+		}
+
+		public boolean hasNext() {
+			try {
+				while (st == null || !st.hasMoreTokens())
+					st = new StringTokenizer(br.readLine());
+
+			} catch (Exception e) {
+				return false;
+			}
+			return true;
+		}
+
+		public boolean ready() throws IOException {
+			return br.ready();
+		}
+
+	}
