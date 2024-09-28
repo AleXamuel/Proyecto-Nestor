f2fa3364c347bed6259effaa903cd16c1f72ff6b
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2024-09-28 17:49:26 -0500

    Create Dijkstra.java

---
+++ Algoritmos/Java/Grafos/Dijkstra.java
@@ -0,0 +1,51 @@
+static boolean[] state;
+static ArrayList<Edge>[] adj;
+static int[] d;
+static int[] pi;
+static int INF =Integer.MAX_VALUE;
+
+
+static void dijkstra(int s, int t) {
+    Arrays.fill(d, INF);
+    d[s] = 0;
+    TreeSet<Edge> Q = new TreeSet<Edge>();
+    Q.add(new Edge(s, d[s]));
+    while (!Q.isEmpty()) {
+        Edge e = Q.pollFirst();
+        int u = e.vertex;
+        if (!state[u]) {
+            state[u] = true;
+            for (Edge ee : adj[u]) {
+                int v = ee.vertex;
+                int w = ee.weight;
+                if (!state[v])
+                    if (d[u] + w < d[v]) {
+                        d[v] = d[u] + w;
+                        pi[v]=u;
+                        Q.add(new Edge(v, d[v]));
+                    }
+            }
+        }
+    }
+}
+static class Edge implements Comparable<Edge> {
+    int vertex;
+    int weight;
+
+    public Edge(int v, int w) {
+        this.vertex = v;
+        this.weight = w;
+    }
+
+    public int compareTo(Edge o) {
+        if (weight < o.weight)
+            return -1;
+        if (weight > o.weight)
+            return 1;
+        if (vertex < o.vertex)
+            return -1;
+        if (vertex > o.vertex)
+            return 1;
+        return 0;
+    }
+}
