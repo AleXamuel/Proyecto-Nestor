51989cabfb0a8bb496ec6602a49ba329702c3349
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2024-10-05 02:11:47 -0500

    Update Monsters.java
    BFS to the flight

--- Problemas/Grafos/Grid/Monsters.java
+++ Problemas/Grafos/Grid/Monsters.java
@@ -1,9 +1,195 @@
 /*https://vjudge.net/problem/CSES-1194*/
-
 import java.util.*;
 import java.io.*;
 
-public class Monsters {
+
+public class MonstersIan {
+	static char[][] grid;
+
+	static int[][] dA, dM;
+	static boolean[][] stateA, stateM;
+	static Queue<int[]> Q;
+
+	static int[][][] pi;
+
+	static int n, m;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		grid = new char[n][m];
+		dM = new int[n][m];
+		dA = new int[n][m];
+		stateA = new boolean[n][m];
+		stateM = new boolean[n][m];
+		for (int i = 0; i < n; i++)
+			grid[i] = in.readLine().toCharArray();
+		for (int[] is : dA)
+			Arrays.fill(is, Integer.MAX_VALUE);
+		for (int[] is : dM)
+			Arrays.fill(is, Integer.MAX_VALUE);
+		int[] me = new int[2];
+		List<int[]> salidas = new ArrayList<>();
+		Q = new LinkedList<>();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] == 'M') {
+					Q.add(new int[] { i, j });
+					dM[i][j] = 0;
+					stateM[i][j] = true;
+				}
+				if (grid[i][j] == 'A') {
+					me[0] = i;
+					me[1] = j;
+				}
+				if (grid[i][j] == '.' && (i == 0 || i == n - 1 || j == 0 || j == m - 1))
+					salidas.add(new int[] { i, j });
+			}
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				if (grid[i][j] == 'A' && (i == 0 || i == n - 1 || j == 0 || j == m - 1)) {
+					System.out.println("YES");
+					System.out.println(0);
+					return;
+				}
+		if (salidas.isEmpty()) {
+			System.out.println("NO");
+			return;
+		}
+		bfsM();
+		bfsA(me[0], me[1]);
+		boolean cond = false;
+		for (int[] i : salidas) {
+			int r = i[0];
+			int c = i[1];
+			if (dA[r][c] < dM[r][c]) {
+				System.out.println("YES");
+				System.out.println(dA[r][c]);
+				cond = true;
+				List<int[]> path = buildPath(r, c);
+				path.remove(0);
+				System.out.println(printPath(path, me[0], me[1]));
+				break;
+			}
+		}
+		if (!cond) {
+			System.out.println("NO");
+		}
+
+	}
+
+	static String printPath(List<int[]> path, int si, int sj) {
+		StringBuffer buf = new StringBuffer();
+		for (int[] i : path) {
+			int r = i[0];
+			int c = i[1];
+			if (r == si)
+				buf.append(c > sj ? "R" : "L");
+			else if (c == sj)
+				buf.append(r < si ? "U" : "D");
+			else
+				buf.append(r > si ? "D" : "U");
+			si = r;
+			sj = c;
+		}
+		return new String(buf);
+
+	}
+
+	static List<int[]> buildPath(int ti, int tj) {
+		List<int[]> path = new LinkedList<>();
+		path.add(new int[] { ti, tj });
+		while (ti != -1 && tj != -1) {
+			int aux = ti;
+			ti = pi[ti][tj][0];
+			tj = pi[aux][tj][1];
+			path.add(0, new int[] { ti, tj });
+		}
+		path.remove(0);
+		return path;
+	}
+
+	static void bfsA(int i, int j) {
+		pi = new int[n][m][2];
+		for (int[][] u : pi)
+			for (int[] v : u)
+				Arrays.fill(v, -1);
+		Q = new LinkedList<>();
+		dA[i][j] = 0;
+		stateA[i][j] = true;
+		Q.add(new int[] { i, j });
+		while (!Q.isEmpty()) {
+			int[] e = Q.poll();
+			int r = e[0];
+			int c = e[1];
+			if (r - 1 >= 0 && !stateA[r - 1][c] && grid[r - 1][c] == '.') {
+				stateA[r - 1][c] = true;
+				dA[r - 1][c] = dA[r][c] + 1;
+				pi[r - 1][c] = new int[] { r, c };
+				Q.add(new int[] { r - 1, c });
+			}
+			if (r + 1 < n && !stateA[r + 1][c] && grid[r + 1][c] == '.') {
+				stateA[r + 1][c] = true;
+				dA[r + 1][c] = dA[r][c] + 1;
+				pi[r + 1][c] = new int[] { r, c };
+				Q.add(new int[] { r + 1, c });
+			}
+			if (c - 1 >= 0 && !stateA[r][c - 1] && grid[r][c - 1] == '.') {
+				stateA[r][c - 1] = true;
+				dA[r][c - 1] = dA[r][c] + 1;
+				pi[r][c - 1] = new int[] { r, c };
+				Q.add(new int[] { r, c - 1 });
+			}
+			if (c + 1 < m && !stateA[r][c + 1] && grid[r][c + 1] == '.') {
+				stateA[r][c + 1] = true;
+				dA[r][c + 1] = dA[r][c] + 1;
+				pi[r][c + 1] = new int[] { r, c };
+				Q.add(new int[] { r, c + 1 });
+			}
+		}
+	}
+
+	static void bfsM() {
+		while (!Q.isEmpty()) {
+			int[] e = Q.poll();
+			int r = e[0];
+			int c = e[1];
+			if (r - 1 >= 0 && !stateM[r - 1][c] && grid[r - 1][c] != '#') {
+				stateM[r - 1][c] = true;
+				dM[r - 1][c] = dM[r][c] + 1;
+				Q.add(new int[] { r - 1, c });
+			}
+			if (r + 1 < n && !stateM[r + 1][c] && grid[r + 1][c] != '#') {
+				stateM[r + 1][c] = true;
+				dM[r + 1][c] = dM[r][c] + 1;
+				Q.add(new int[] { r + 1, c });
+			}
+			if (c - 1 >= 0 && !stateM[r][c - 1] && grid[r][c - 1] != '#') {
+				stateM[r][c - 1] = true;
+				dM[r][c - 1] = dM[r][c] + 1;
+				Q.add(new int[] { r, c - 1 });
+			}
+			if (c + 1 < m && !stateM[r][c + 1] && grid[r][c + 1] != '#') {
+				stateM[r][c + 1] = true;
+				dM[r][c + 1] = dM[r][c] + 1;
+				Q.add(new int[] { r, c + 1 });
+			}
+		}
+	}
+
+	static String print(int[][] A) {
+		StringBuffer buf = new StringBuffer();
+		for (int[] i : A)
+			buf.append("[" + i[0] + ", " + i[1] + "]" + ", ");
+		return new String(buf);
+	}
+
+}
+
+
+public class MonstersSamu {
 
     static int[][] d;
     static int[][] da;
