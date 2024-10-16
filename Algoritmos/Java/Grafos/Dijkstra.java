bfc509daf77d579c4dad5f7ccd03b0c9da312a5b
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2024-10-16 10:34:46 -0500

    Update Dijkstra.java

--- Algoritmos/Java/Grafos/Dijkstra.java
+++ Algoritmos/Java/Grafos/Dijkstra.java
@@ -1,51 +1,40 @@
-static boolean[] state;
-static ArrayList<Edge>[] adj;
-static int[] d;
-static int[] pi;
-static int INF =Integer.MAX_VALUE;
-
-
-static void dijkstra(int s, int t) {
-    Arrays.fill(d, INF);
-    d[s] = 0;
-    TreeSet<Edge> Q = new TreeSet<Edge>();
-    Q.add(new Edge(s, d[s]));
-    while (!Q.isEmpty()) {
-        Edge e = Q.pollFirst();
-        int u = e.vertex;
-        if (!state[u]) {
-            state[u] = true;
-            for (Edge ee : adj[u]) {
-                int v = ee.vertex;
-                int w = ee.weight;
-                if (!state[v])
-                    if (d[u] + w < d[v]) {
-                        d[v] = d[u] + w;
-                        pi[v]=u;
-                        Q.add(new Edge(v, d[v]));
-                    }
+import java.util.*;
+public class Pruebas {
+    static boolean[] state;
+    static ArrayList<Edge>[] adj;
+    static int[] d,pi;
+    static int INF =Integer.MAX_VALUE;
+    static void dijkstra(int s, int t) {
+        Arrays.fill(d, INF);
+        d[s] = 0;
+        TreeSet<Edge> Q = new TreeSet<>();
+        Q.add(new Edge(s, d[s]));
+        while (!Q.isEmpty()) {
+            Edge e = Q.pollFirst();
+            int u = e.vertex;
+            if (!state[u]) {
+                state[u] = true;
+                for (Edge ee : adj[u]) {
+                    int v = ee.vertex,w = ee.weight;;
+                    if (!state[v])
+                        if (d[u] + w < d[v]) {
+                            d[v] = d[u] + w;
+                            pi[v]=u;
+                            Q.add(new Edge(v, d[v]));
+                        }
+                }
             }
         }
     }
-}
-static class Edge implements Comparable<Edge> {
-    int vertex;
-    int weight;
-
-    public Edge(int v, int w) {
-        this.vertex = v;
-        this.weight = w;
-    }
-
-    public int compareTo(Edge o) {
-        if (weight < o.weight)
-            return -1;
-        if (weight > o.weight)
-            return 1;
-        if (vertex < o.vertex)
-            return -1;
-        if (vertex > o.vertex)
-            return 1;
-        return 0;
+    static class Edge implements Comparable<Edge> {
+        int vertex,weight;
+        public Edge(int v, int w) {
+            this.vertex = v;
+            this.weight = w;
+        }
+        public int compareTo(Edge o) {
+            if(weight!=o.weight)return Integer.compare(weight,o.weight);
+            return Integer.compare(vertex,o.vertex);
+        }
     }
 }
