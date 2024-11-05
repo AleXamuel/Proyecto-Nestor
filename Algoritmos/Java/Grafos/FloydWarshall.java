b96cae70d3d517b95ea4174633ef0a324460967b
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2024-11-04 21:36:03 -0500

    Update FloydWarshall.java

--- Algoritmos/Java/Grafos/FloydWarshall.java
+++ Algoritmos/Java/Grafos/FloydWarshall.java
@@ -1,9 +1,23 @@
-static void floydWarshall(int[][] res) {
-    int N=res.length;
-    for (int k = 0; k < N; k++)
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                if (res[i][k] < Integer.MAX_VALUE && res[k][j] < Integer.MAX_VALUE)
-                    res[i][j] = Math.min(res[i][j], res[i][k] + res[k][j]);
+public class FloydWarshall {
+		static int n;
+		static int[][] adj;
 
-}
+		static void initializeGraph() {
+			adj = new int[n][n];
+			for (int[] i : adj)
+				Arrays.fill(i, Integer.MAX_VALUE);
+		}
+
+		static void addEdge(int source, int destination, int cost) {
+			adj[source][destination] = cost;
+		}
+
+		static void floydWarshall() {
+			for (int k = 0; k < n; k++)
+				for (int i = 0; i < n; i++)
+					for (int j = 0; j < n; j++)
+						if (adj[i][k] < Integer.MAX_VALUE && adj[k][j] < Integer.MAX_VALUE)
+							adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+
+		}
+	}
