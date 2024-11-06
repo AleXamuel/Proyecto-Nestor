d83d721544a1336b29e02ddde885f6b583311ac5
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2024-11-05 23:23:32 -0500

    Create TouringRobot.py

---
+++ Problemas/Geometría Computacional/TouringRobot.py
@@ -0,0 +1,40 @@
+#https://onlinejudge.org/contests/211-7b52419c/11509.html
+import math
+
+def calcular_angulo(x1, y1, x2, y2):
+    return math.atan2(y2 - y1, x2 - x1)
+
+def angulo_rotacion(angulo1, angulo2):
+    rotacion = angulo2 - angulo1
+    if rotacion < 0:
+        rotacion += 2 * math.pi
+    return rotacion
+
+def calcular_giro(puntos):
+    n = len(puntos)
+    if n < 2:
+        return 0
+    angulo_total = 0.0
+    for i in range(n):
+        x1, y1 = puntos[i]
+        x2, y2 = puntos[(i + 1) % n]
+        x3, y3 = puntos[(i + 2) % n]
+
+        angulo1 = calcular_angulo(x1, y1, x2, y2)
+        angulo2 = calcular_angulo(x2, y2, x3, y3)
+
+        rotacion = angulo_rotacion(angulo1, angulo2)
+        rotacion_grados = math.degrees(rotacion)
+        if rotacion_grados > 0:  # solo contar giros positivos
+            angulo_total += rotacion_grados
+    return round(angulo_total / 360)  # número de giros completos
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    puntos = []
+    for i in range(n):
+        x, y = map(int, input().split())
+        puntos.append((x, y))
+    print(calcular_giro(puntos))
