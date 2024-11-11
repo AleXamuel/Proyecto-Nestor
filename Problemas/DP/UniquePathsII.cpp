231c4bc2f696b676ef3893ffd972ebd01f247dfe
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2024-11-11 10:23:59 -0500

    Create UniquePathsII.cpp

---
+++ Problemas/DP/UniquePathsII.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/unique-paths-ii/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid.empty() || obstacleGrid[0].empty()) {
+            return 0;
+        }
+
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = dp[i - 1][0];
+            } else {
+                break;
+            }
+        }
+
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                dp[0][j] = dp[0][j - 1];
+            } else {
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
