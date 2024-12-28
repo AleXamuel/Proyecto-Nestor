7ff9423c95728e4788be1bc287e08193454f5e75
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2024-12-28 00:00:20 -0500

    Update (a^p)%m.cpp

--- Algoritmos/C++/Module/(a^p)%m.cpp
+++ Algoritmos/C++/Module/(a^p)%m.cpp
@@ -1,7 +1,7 @@
-long bigMod(long a, long p, long m) {
-    long res = 1%m,x=a%m;
-    while (p) {
-        if (p & 1) res=(res*x)%m;
+long long bigMod(long a, long p, long m) {
+    long long res = 1%m,x=a%m;
+    while (p>0) {
+        if ((p & 1)>0) res=(res*x)%m;
         x = (x*x)%m;
         p >>= 1;
     }
