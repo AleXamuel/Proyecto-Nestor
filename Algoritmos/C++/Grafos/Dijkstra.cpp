f65dec55fae90529c108087b741e6ec418ca9c61
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-01-05 13:40:37 -0500

    Update Dijkstra.cpp

--- Algoritmos/C++/Grafos/Dijkstra.cpp
+++ Algoritmos/C++/Grafos/Dijkstra.cpp
@@ -1,21 +1,22 @@
+
 typedef unsigned long long ll;
-typedef pair<ll, ll> Pair;
+typedef pair<ll, int> Pair;
 ll INF=18446744073709551615;
-vector<ll> dijkstra(int source, vector<vector<Pair>>& graph, vector<ll>& d) {
+vector<ll> dijkstra(int source, vector<vector<Pair>>& graph) {
     int n = graph.size();
-    d.assign(n, numeric_limits<ll>::max());
+    vector<ll> d(n, INF);
     d[source] = 0;
     priority_queue<Pair, vector<Pair>, greater<Pair>> pq;
     vector<bool> state(n, false);
     pq.push({0, source});
     while (!pq.empty()) {
-        ll u = pq.top().second;
+        int u = pq.top().second;
         pq.pop();
         if (state[u]) continue;
         state[u] = true;
         for (const Pair& edge : graph[u]) {
-            ll v = edge.first;
-            ll w = edge.second;
+            ll w = edge.first;
+            int v = edge.second;
             if(!state[v]) {
                 if (d[u] + w < d[v]) {
                     d[v] = d[u] + w;
