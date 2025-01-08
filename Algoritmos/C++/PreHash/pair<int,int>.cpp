66dbd33d5d37fa8198cb0618061dbb6e65ca72bf
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-01-08 18:35:25 -0500

    Create pair<int,int>.cpp

---
+++ Algoritmos/C++/PreHash/pair<int,int>.cpp
@@ -0,0 +1,8 @@
+struct PairHash {
+    template <typename T1, typename T2>
+    size_t operator()(const pair<T1, T2>& p) const {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2*31;
+    }
+};
