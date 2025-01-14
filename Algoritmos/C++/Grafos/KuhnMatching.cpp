ff09a80b7cc6e7c86168ae3895c25a67014c98f3
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-01-14 14:26:41 -0500

    Create KuhnMatching.cpp

---
+++ Algoritmos/C++/Grafos/KuhnMatching.cpp
@@ -0,0 +1,15 @@
+vector<vector<int>> adj;
+vector<int> mt;
+vector<bool> used;
+bool try_kuhn(int v) {
+    if (used[v])
+        return false;
+    used[v] = true;
+    for (int to : adj[v]) {
+        if (mt[to] == -1 || try_kuhn(mt[to])) {
+            mt[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
