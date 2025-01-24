8d614db7e5a84cb55cefed1671640d1b6f9e7dcb
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-01-24 11:15:16 -0500

    Gray_Code.py

--- Problemas/Bitmask/Gray_Code.py
+++ Problemas/Bitmask/Gray_Code.py
@@ -2,5 +2,5 @@
 n = int(input())
 for i in range(1 << n):
  
-    binary = bin(i ^ i >> 1)[2:].zfill(n)
+    binary = bin(i ^ i >> 1)[2:].zfill(n) #zfill rellena de zeros a la izquierda n veces, si no tiene nada, si no lo omite.
     print(binary)
