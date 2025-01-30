80b5e8c8226ef39c56b3853ef8315c2a1b8e9167
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-01-30 13:54:49 -0500

    Update mod_exponential.cpp

--- Algoritmos/C++/Math/mod_exponential.cpp
+++ Algoritmos/C++/Math/mod_exponential.cpp
@@ -1,15 +1,10 @@
 const int MOD = 1000000007;
-
-long long mod_exp(int base, int exp, int mod) {
-    long long result = 1;
-    long long power = base % mod; 
-
-    while (exp > 0) {
-        if (exp % 2 == 1) { 
-            result = (result * power) % mod; 
-        }
-        power = (power * power) % mod; 
-        exp /= 2; 
+long long modExp(long base, long exp, long mod) {
+    long long res = 1,x=base%mod;
+    while (exp>0) {
+        if ((exp & 1)>0) res=(res*x)%mod;
+        x = (x*x)%mod;
+        exp >>= 1;
     }
-    return result;
+    return res;
 }
