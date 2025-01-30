d5af2969d47d1d10a26ed1720d197d812cb4566b
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-01-30 13:54:57 -0500

    Delete Algoritmos/C++/Module directory

--- Algoritmos/C++/Module/(a^p)%m.cpp
+++
@@ -1,9 +0,0 @@
-long long bigMod(long a, long p, long m) {
-    long long res = 1%m,x=a%m;
-    while (p>0) {
-        if ((p & 1)>0) res=(res*x)%m;
-        x = (x*x)%m;
-        p >>= 1;
-    }
-    return res;
-}
