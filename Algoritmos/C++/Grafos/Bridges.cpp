68671d5827374644e1dc469df4a3d126ded16278
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-02-15 20:55:22 -0500

    Create Bridges.cpp

---
+++ Algoritmos/C++/Grafos/Bridges.cpp
@@ -0,0 +1,29 @@
+#define Pair pair<int,int>
+#define eb emplace_back
+int n;
+int t;
+vector<int> d;
+vector<int> low;
+vector<vector<int>> adj;
+vector<Pair>bridge;
+void dfs(int u, int par) {
+    d[u] = t++;
+    low[u] = d[u];
+    for(int v:adj[u]) {
+        if (v==par) continue;
+        if (d[v]>0)
+            low[u] = min(low[u], d[v]);
+        else {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v]>d[u]) bridge.eb(make_pair(u,v));
+        }
+    }
+}
+void get_bridges() {
+    d.assign(n, 0);
+    low.assign(n,0);
+    t = 1;
+    bridge.clear();
+    dfs(0, -1);
+}
