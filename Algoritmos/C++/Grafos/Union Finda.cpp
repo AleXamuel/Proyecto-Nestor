f8b6d69c372926ac5a393eb2b8896112dc79d2b9
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-02-15 20:56:33 -0500

    Create Union Finda.cpp

---
+++ Algoritmos/C++/Grafos/Union Finda.cpp
@@ -0,0 +1,25 @@
+vector<int> d, low, pi, Size;
+int Find(int u) {
+    int aux = u;
+    while (u != pi[u])
+        u = pi[u];
+    while (aux != pi[aux]) {
+        aux = pi[aux];
+        pi[aux] = u;
+    }
+    return u;
+}
+
+void Union(int u, int v) {
+    int x = Find(u);
+    int y = Find(v);
+    if (x == y)
+        return;
+    if (Size[x] < Size[y]) {
+        pi[x] = y;
+        Size[y] += Size[x];
+    } else {
+        pi[y] = x;
+        Size[x] += Size[y];
+    }
+}
