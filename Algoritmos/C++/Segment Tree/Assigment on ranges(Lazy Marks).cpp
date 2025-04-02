3bda1e8e40eeb7e682b7c9a9b9c85242562cf26f
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-04-02 18:17:38 -0500

    Assigment on ranges(Lazy Marks).cpp

---
+++ Algoritmos/C++/Segment Tree/Assigment on ranges(Lazy Marks).cpp
@@ -0,0 +1,36 @@
+int t[4 * MAXN];
+bool marked[4 * MAXN];
+
+void push(int v) {
+    if (marked[v]) {
+        t[v*2] = t[v*2+1] = t[v];
+        marked[v*2] = marked[v*2+1] = true;
+        marked[v] = false;
+    }
+}
+
+void update(int v, int tl, int tr, int l, int r, int new_val) {
+    if (l > r)
+        return;
+    if (l == tl && tr == r) {
+        t[v] = new_val; //assign 
+        marked[v] = true;
+    } else {
+        push(v);
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), new_val);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, new_val);
+    }
+}
+
+int get(int v, int tl, int tr, int pos) {
+    if (tl == tr) {
+        return t[v];
+    }
+    push(v);
+    int tm = (tl + tr) / 2;
+    if (pos <= tm)
+        return get(v*2, tl, tm, pos);
+    else
+        return get(v*2+1, tm+1, tr, pos);
+}
