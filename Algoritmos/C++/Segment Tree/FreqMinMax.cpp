3b613b1e7f1742e94a13940cf99252854afa211f
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-04-01 19:02:39 -0500

    Update FreqMinMax.cpp

--- Algoritmos/C++/Segment Tree/FreqMinMax.cpp
+++ Algoritmos/C++/Segment Tree/FreqMinMax.cpp
@@ -1,23 +1,17 @@
 //Compute the Maximum or Minimum of a given segment of an Array and the number of times it appears on that segment.
-
 using namespace std;
 #include <bits/stdc++.h>
 #define MAXN 100000
-
 int n;
 pair<long long, int> t[4 * MAXN];
-
-pair<long long, int> combine(pair<long long, int> a, pair<long long, int> b)
-{
+pair<long long, int> combine(pair<long long, int> a, pair<long long, int> b){
     if (a.first < b.first)
         return a;
     if (b.first < a.first)
         return b;
     return {a.first, a.second + b.second};
 }
-
-void build(long long a[], int v = 1, int tl = 0, int tr = n - 1)
-{
+void build(long long a[], int v = 1, int tl = 0, int tr = n - 1){
     if (tl == tr)
         t[v] = {a[tl], 1};
     else
@@ -28,9 +22,7 @@ void build(long long a[], int v = 1, int tl = 0, int tr = n - 1)
         t[v] = combine(t[v * 2], t[v * 2 + 1]);
     }
 }
-
-pair<long long, int> query(int v, int tl, int tr, int l, int r)
-{
+pair<long long, int> query(int v, int tl, int tr, int l, int r){
     if (l > r)
         return {INT_MAX, 0};
     if (l == tl && r == tr)
@@ -39,9 +31,7 @@ pair<long long, int> query(int v, int tl, int tr, int l, int r)
     return combine(query(v * 2, tl, tm, l, min(r, tm)),
                    query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
 }
-
-void update(int v, int tl, int tr, int pos, int new_val)
-{
+void update(int v, int tl, int tr, int pos, int new_val){
     if (tl == tr)
         t[v] = {new_val, 1};
     else
