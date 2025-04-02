c0fd3fb43112321684dd143f1a20667151c670a5
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-04-01 19:21:52 -0500

    Create Apply permutations.cpp

---
+++ Algoritmos/C++/Math/Apply permutations.cpp
@@ -0,0 +1,18 @@
+vector<int> applyPermutation(vector<int> sequence, vector<int> permutation) {
+    vector<int> newSequence(sequence.size());
+    for(int i = 0; i < sequence.size(); i++) {
+        newSequence[i] = sequence[permutation[i]];
+    }
+    return newSequence;
+}
+
+vector<int> permute(vector<int> sequence, vector<int> permutation, long long k) {
+    while (k > 0) {
+        if (k & 1) {
+            sequence = applyPermutation(sequence, permutation);
+        }
+        permutation = applyPermutation(permutation, permutation);
+        k >>= 1;
+    }
+    return sequence;
+}
