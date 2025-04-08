09e9b8b59aef92bfbcf674b5cc9af76062474824
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-04-07 22:09:48 -0500

    Decode The Tape.cpp

---
+++ Problemas/Strings/Decode The Tape.cpp
@@ -0,0 +1,34 @@
+//https://vjudge.net/problem/UVA-10878
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define ln "\n"
+
+void solve() {
+    string s;
+    getline(cin, s);
+    while (true) {
+        getline(cin, s);
+        if (s[0] == '_')
+            break;
+        string bin = "";
+        int n = s.size();
+        For(i, 0, n) {
+            if (s[i] == ' ')
+                bin += '0';
+            else if (s[i] == 'o')
+                bin += '1';
+        }
+        char ch = (char)stoi(bin,nullptr, 2);
+        cout << ch;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
+
