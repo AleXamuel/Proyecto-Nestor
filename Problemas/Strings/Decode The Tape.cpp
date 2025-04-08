922dcaaae0ea2f836375dfe255712954f0997960
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-04-07 22:11:10 -0500

    Decode The Tape.cpp

--- Problemas/Strings/Decode The Tape.cpp
+++ Problemas/Strings/Decode The Tape.cpp
@@ -2,6 +2,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define ull unsigned long long
+#define For(i, k, n) for (int i = k; i < n; i++)
+#define Rfor(i, n, k) for (int i = n - 1; i >= k; i--)
 #define all(v) v.begin(), v.end()
 #define ln "\n"
 
