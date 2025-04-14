ffb3ace0e04931f4a39aa0f4e32ce86ae9ce6354
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-04-13 21:38:18 -0500

    Create BellmandFord.cpp

---
+++ Algoritmos/C++/Grafos/BellmandFord.cpp
@@ -0,0 +1,41 @@
+struct Edge {
+    int a, b, cost;
+};
+
+int n, m, v;
+vector<Edge> edges;
+const int INF = 1000000000;
+void bellmand(){
+    vector<int> d(n, INF);
+    d[v] = 0;
+    vector<int> p(n, -1);
+    int x;
+    for (int i = 0; i < n; ++i) {
+        x = -1;
+        for (Edge e : edges)
+            if (d[e.a] < INF)
+                if (d[e.b] > d[e.a] + e.cost) {
+                    d[e.b] = max(-INF, d[e.a] + e.cost);
+                    p[e.b] = e.a;
+                    x = e.b;
+                }
+    }
+
+    if (x == -1)
+        cout << "No negative cycle from " << v;
+    else {
+        int y = x;
+        for (int i = 0; i < n; ++i)
+            y = p[y];
+        vector<int> path;
+        for (int cur = y;; cur = p[cur]) {
+            path.push_back(cur);
+            if (cur == y && path.size() > 1)
+                break;
+        }
+        reverse(path.begin(), path.end());
+        cout << "Negative cycle: ";
+        for (int u : path)
+            cout << u << ' ';
+    }
+}
