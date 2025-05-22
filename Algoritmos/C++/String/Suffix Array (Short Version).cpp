f6f26e88725bf73a752cf219a2183e5d5551c247
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-05-22 11:34:24 -0500

    Update Suffix Array (Short Version).cpp

--- Algoritmos/C++/String/Suffix Array (Short Version).cpp
+++ Algoritmos/C++/String/Suffix Array (Short Version).cpp
@@ -70,8 +70,3 @@ vector<int> lcp_construction(string const& s, vector<int> const& p) {
     }
     return lcp;
 }
-int compare(int i, int j, int l, int k) {
-    pair<int, int> a = {c[k][i], c[k][(i+l-(1 << k))%n]};
-    pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
-    return a == b ? 0 : a < b ? -1 : 1;
-}
