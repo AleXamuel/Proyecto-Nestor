f84161f795dfd221f8271074490aa731a7ea7915
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-06-03 00:50:29 -0500

    Create Counting Patterns.cpp

---
+++ Problemas/Strings/Suffix Array/Counting Patterns.cpp
@@ -0,0 +1,149 @@
+//https://cses.fi/problemset/task/2103
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template<class T>
+using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define dll double long
+#define ll long long
+#define Pair pair<int,int>
+#define eb emplace_back
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ln "\n"
+#define mk(x,y) make_pair(x,y)
+#define For(i, a, b) for(int i = a; i < b; i++)
+#define Rfor(i, a, b) for(int i = a; i >= b; i--)
+vector<Pair > ch = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const int MOD = 1000000007;
+const ll inf = -2e18;
+//solution
+vector<int> sort_cyclic_shifts(string const& s) {
+    int n = s.size();
+    const int alphabet = 256;
+    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
+    for (int i = 0; i < n; i++)
+        cnt[s[i]]++;
+    for (int i = 1; i < alphabet; i++)
+        cnt[i] += cnt[i-1];
+    for (int i = 0; i < n; i++)
+        p[--cnt[s[i]]] = i;
+    c[p[0]] = 0;
+    int classes = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[p[i]] != s[p[i-1]])
+            classes++;
+        c[p[i]] = classes - 1;
+    }
+    vector<int> pn(n), cn(n);
+    for (int h = 0; (1 << h) < n; ++h) {
+        for (int i = 0; i < n; i++) {
+            pn[i] = p[i] - (1 << h);
+            if (pn[i] < 0)
+                pn[i] += n;
+        }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (int i = 0; i < n; i++)
+            cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++)
+            cnt[i] += cnt[i-1];
+        for (int i = n-1; i >= 0; i--)
+            p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+            if (cur != prev)
+                ++classes;
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+    }
+    return p;
+}
+vector<int> suffix_array_construction(string s) {
+    s += "$";
+    vector<int> sorted_shifts = sort_cyclic_shifts(s);
+    sorted_shifts.erase(sorted_shifts.begin());
+    return sorted_shifts;
+}
+vector<int> lcp_construction(string const& s, vector<int> const& p) {
+    int n = s.size();
+    vector<int> rank(n, 0);
+    for (int i = 0; i < n; i++)
+        rank[p[i]] = i;
+ 
+    int k = 0;
+    vector<int> lcp(n-1, 0);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        int j = p[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i+k] == s[j+k])
+            k++;
+        lcp[rank[i]] = k;
+        if (k)
+            k--;
+    }
+    return lcp;
+}
+vector<int>p;
+int f(int i,string& s,string& t) {
+    For(j,0,t.size()) {
+        if(s[i]>t[j])
+            return -1;
+        if(s[i++]<t[j])
+            return 1;
+    }
+    return 0;
+}
+int lower(string& s,string& t) {
+    int l=0,r=s.size()-1;
+    int ans=-1;
+    while(l<=r) {
+        int mid=l+r>>1;
+        int c=f(p[mid],s,t);
+        if(c==0)ans=mid;
+        if(c<=0)r=mid-1;
+        else
+            l=mid+1;
+    }
+    return ans;
+}
+int upper(string& s,string& t) {
+    int l=0,r=s.size()-1;
+    int ans=-1;
+    while(l<=r) {
+        int mid=l+r>>1;
+        int c=f(p[mid],s,t);
+        if(c==0)ans=mid;
+        if(c<0)r=mid-1;
+        else
+            l=mid+1;
+    }
+    return ans;
+}
+void solve() {
+    string s;cin>>s;
+    p=suffix_array_construction(s);
+    int q;cin>>q;
+    while(q--) {
+        string t;cin>>t;
+        int l=lower(s,t),r=upper(s,t);
+        if(l==-1)cout<<0<<ln;
+        else
+            cout<<r-l+1<<ln;
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int ti = 1;
+    solve();
+    return 0;
+}
