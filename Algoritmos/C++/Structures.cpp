f8e83635c9d111636f2c0b8f1aca749dac87a85b
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-08-09 10:58:36 -0500

    Update Structures.cpp

--- Algoritmos/C++/Structures.cpp
+++ Algoritmos/C++/Structures.cpp
@@ -4,3 +4,6 @@ using namespace __gnu_pbds;
 template <typename T>
 using i_set =
     tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order(i): return the value in the position i if it was an ordered array
+// order_of_key(x): return the index of the element x if it was an ordered array
+
