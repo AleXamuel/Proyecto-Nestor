6ae1ad3dc594607427bf1d457be96be063b022fb
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-08-11 20:17:21 -0500

    Recursion, Brute Force, Backtracking

---
+++ Problemas/Problems/LeetCode 52. N-Queens II.cpp
@@ -0,0 +1,68 @@
+//https://leetcode.com/problems/n-queens-ii/description/
+#define For(i, k, n) for (int i = k; i < n; i++)
+#define ln "\n"
+class Solution {
+public:
+    vector<vector<int>> state;
+    int n;
+    void rellenar(int x, int y) {
+        For(i, 0, n) state[x][i]++;
+        For(i, 0, n) if (i != x) state[i][y]++;
+        int i = x + 1;
+        int j = y + 1;
+        while (i < n && j < n)
+            state[i++][j++]++;
+        i = x - 1;
+        j = y - 1;
+        while (i >= 0 && j >= 0)
+            state[i--][j--]++;
+        i = x + 1;
+        j = y - 1;
+        while (i < n && j >= 0)
+            state[i++][j--]++;
+        i = x - 1;
+        j = y + 1;
+        while (i >= 0 && j < n)
+            state[i--][j++]++;
+    }
+
+    void desrellenar(int x, int y) {
+        For(i, 0, n) state[x][i]--;
+        For(i, 0, n) if (i != x) state[i][y]--;
+        int i = x + 1;
+        int j = y + 1;
+        while (i < n && j < n)
+            state[i++][j++]--;
+        i = x - 1;
+        j = y - 1;
+        while (i >= 0 && j >= 0)
+            state[i--][j--]--;
+        i = x + 1;
+        j = y - 1;
+        while (i < n && j >= 0)
+            state[i++][j--]--;
+        i = x - 1;
+        j = y + 1;
+        while (i >= 0 && j < n)
+            state[i--][j++]--;
+    }
+    int f(int i) {
+        if (i == n) 
+            return 1;
+        int ans = 0;
+        For(j, 0, n) {
+            if (state[i][j] == 0) {
+                rellenar(i, j);
+                ans+=f(i + 1);
+                desrellenar(i, j);
+            }
+        }
+        return ans;
+    }
+
+    int totalNQueens(int N) {
+        n = N;
+        state.assign(n, vector<int>(n, 0));
+        return f(0);
+    }
+};
