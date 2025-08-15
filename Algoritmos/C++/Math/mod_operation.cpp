bcf638dac6a5a7ee7cee4fff7097db4a042b5e68
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-08-15 09:59:34 -0500

    Update mod_operation.cpp

--- Algoritmos/C++/Math/mod_operation.cpp
+++ Algoritmos/C++/Math/mod_operation.cpp
@@ -22,3 +22,28 @@ int binpow(int x, int y) {
 int divide(int x, int y) {
     return mul(x, binpow(y, MOD - 2));
 }
+template<int MOD> struct mint {
+	static const int mod = MOD;
+ 	int v;
+ 	explicit operator int() const { return v; }
+	mint():v(0) {}
+	mint(ll _v):v(int(_v%MOD)) { v += (v<0)*MOD; }
+	mint& operator/=(const mint& o) { return (*this) *= inv(o); }
+	mint& operator+=(mint o) {
+		if ((v += o.v) >= MOD) v -= MOD;
+		return *this; }
+	mint& operator-=(mint o) {
+		if ((v -= o.v) < 0) v += MOD;
+		return *this; }
+	mint& operator*=(mint o) {
+		v = int((ll)v*o.v%MOD); return *this; }
+	friend mint pow(mint a, ll p) { assert(p >= 0);
+		return p==0?1:pow(a*a,p/2)*(p&1?a:1); }
+	friend mint inv(mint a) { assert(a.v != 0); return pow(a,MOD-2); }
+	friend mint operator+(mint a, mint b) { return a += b; }
+	friend mint operator-(mint a, mint b) { return a -= b; }
+	friend mint operator*(mint a, mint b) { return a *= b; }
+	friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+ 
+using mi = mint<MOD>;
