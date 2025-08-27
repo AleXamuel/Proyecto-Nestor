286185dfdf7d03d0e27da962e19151965ca28feb
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-08-26 19:58:03 -0500

    Update Aho-Corasick.cpp

--- Algoritmos/C++/String/Aho-Corasick.cpp
+++ Algoritmos/C++/String/Aho-Corasick.cpp
@@ -67,3 +67,76 @@ struct Aho{
     }
 
 };
+struct Aho {
+    static constexpr int K = 52;
+    int n, rt, chd[nmax][K], fail[nmax], q[nmax], hd, tl, en[nmax];
+
+    int newNode() {
+        n++;
+        fill_n(chd[n], K, 0);
+        fail[n] = 0;
+        en[n] = inf;
+        return n;
+    }
+
+    void init() {
+        n = 0;
+        rt = newNode();
+        fail[rt] = rt;
+    }
+
+    int get(char c) {
+        return c < 'a' ? c + 26 - 'A' : c - 'a';
+    }
+
+    void insert(string &s, int k) {
+        int u, i, c;
+        int sn = s.size();
+        u = rt;
+        for (i = 0; i < sn; i++) {
+            c = get(s[i]);
+            if (!chd[u][c])
+                chd[u][c] = newNode();
+            u = chd[u][c];
+        }
+        en[u] = min(en[u], k);
+    }
+
+    void build() {
+        int c, u, v, w;
+        hd = tl = 0;
+        for (c = 0; c < K; c++) {
+            u = chd[rt][c];
+            if (!u)continue;
+            fail[u] = rt;
+            q[tl++] = u;
+        }
+        while (hd < tl) {
+            u = q[hd++];
+            en[u] = min(en[u], en[fail[u]]);
+            for (c = 0; c < K; c++) {
+                v = chd[u][c];
+                if (!v)continue;
+                for (w = fail[u]; w != rt && !chd[w][c]; w = fail[w]);
+                if (chd[w][c])w = chd[w][c];
+                fail[v] = w;
+                q[tl++] = v;
+            }
+        }
+    }
+
+    int find(string &s) {
+        int len = s.length();
+        int u = rt;
+        int ans = inf;
+        for (int i = 0; i < len; i++) {
+            int c = get(s[i]);
+            while (u != rt && !chd[u][c])
+                u = fail[u];
+            if (chd[u][c])
+                u = chd[u][c];
+            ans = min(ans, en[u]);
+        }
+        return ans;
+    }
+};
