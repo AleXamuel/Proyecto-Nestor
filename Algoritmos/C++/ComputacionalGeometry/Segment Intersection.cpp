24985344d56e548e9bde498ee5301db4beee394f
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-09-03 16:23:30 -0500

    Update Segment Intersection.cpp

--- Algoritmos/C++/ComputacionalGeometry/Segment Intersection.cpp
+++ Algoritmos/C++/ComputacionalGeometry/Segment Intersection.cpp
@@ -1,5 +1,10 @@
 struct Point {
     long long x, y;
+    Point() {}
+    Point(long long _x, long long _y) : x(_x), y(_y) {}
+    Point operator-(const Point& p) const { return Point(x - p.x, y - p.y); }
+    long long cross(const Point& p) const { return x * p.y - y * p.x; }
+    long long cross(const Point& a, const Point& b) const { return (a - *this).cross(b - *this); }
     bool operator==(const Point& other) const {
         return x == other.x && y == other.y;
     }
@@ -7,37 +12,21 @@ struct Point {
 struct segment {
     Point a,b;
 };
-int ori(Point a, Point b, Point c) {
-    long long val = (b.y - a.y) * (c.x - b.x) -
-                    (b.x - a.x) * (c.y - b.y);
-    if (val == 0) return 0;
-    return (val > 0) ? 1 : 2;
+
+
+int sgn(const long long& x) { return x >= 0 ? x ? 1 : 0 : -1; }
+
+bool inter1(long long a, long long b, long long c, long long d) {
+    if (a > b)
+        swap(a, b);
+    if (c > d)
+        swap(c, d);
+    return max(a, c) <= min(b, d);
 }
 
-bool inSeg(Point p, Point q, Point r) {
-    return (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
-            q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y));
-}
-
-bool inttersect(Point p1, Point q1, Point p2, Point q2) {
-    int o1 = ori(p1, q1, p2);
-    int o2 = ori(p1, q1, q2);
-    int o3 = ori(p2, q2, p1);
-    int o4 = ori(p2, q2, q1);
-    if (o1 != o2 && o3 != o4) return true;
-    if (o1 == 0 && inSeg(p1, p2, q1)) return true;
-    if (o2 == 0 && inSeg(p1, q2, q1)) return true;
-    if (o3 == 0 && inSeg(p2, p1, q2)) return true;
-    if (o4 == 0 && inSeg(p2, q1, q2)) return true;
-    return false;
-}
-//if they touch, they touch in the extreme points?
-bool bound(Point p1, Point q1, Point p2, Point q2) {
-    if (!intersect(p1, q1, p2, q2)) return false;
-    if (inSeg(p2, p1, q2) || inSeg(p2, q1, q2))
-        return true;
-    if (inSeg(p1, p2, q1) || inSeg(p1, q2, q1))
-        return true;
-
-    return false;
+bool check_inter(const Point& a, const Point& b, const Point& c, const Point& d) {
+    if (c.cross(a, d) == 0 && c.cross(b, d) == 0)
+        return inter1(a.x, b.x, c.x, d.x) && inter1(a.y, b.y, c.y, d.y);
+    return sgn(a.cross(b, c)) != sgn(a.cross(b, d)) &&
+           sgn(c.cross(d, a)) != sgn(c.cross(d, b));
 }
