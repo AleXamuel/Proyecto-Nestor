0b6e580c4c135b330749352acbd12e35824db854
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-09-05 19:33:27 -0500

    Update KMP.cpp

--- Algoritmos/C++/String/KMP.cpp
+++ Algoritmos/C++/String/KMP.cpp
@@ -1,17 +1,25 @@
-vector<int> prefix_function(string s) {
-    int n = (int)s.length();
-    vector<int> pi(n);
+int ans[10001];
+vector<int> kmp(const string& S) {
+    int n=S.size();
+    vector<int> p(n);
     for (int i = 1; i < n; i++) {
-        int j = pi[i-1];
-        while (j > 0 && s[i] != s[j])
-            j = pi[j-1];
-        if (s[i] == s[j])
-            j++;
-        pi[i] = j;
+        int g = p[i - 1];
+        while (g && S[i] != S[g]) 
+            g = p[g - 1];
+        p[i] = g + (S[i] == S[g]);
     }
-    return pi;
+    return p;
 }
-
+struct Automaton : vector<vector<int>> {
+    Automaton(const string &S):vector(26,vector<int>(sz(S)+1)) {
+        auto pi = kmp(S);
+        auto &aut = *this; aut[S[0]-'A'][0] = 1;
+        for (char c = 0; c < 26; c++)
+            for (int i = 1; i <= sz(S); i++)
+                aut[c][i] = S[i]-'A' == c ? i+1 : aut[c][pi[i - 1]];
+    }
+    int nex(char c, int pi) { return (*this)[c-'A'][pi]; }
+};
 //return the frequence of every prefix
 vector<int> ans(n + 1);
 for (int i = 0; i < n; i++)
