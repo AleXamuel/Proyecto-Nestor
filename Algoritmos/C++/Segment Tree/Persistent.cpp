382c5be2bc68406efe4b6fe671af7fff491349bb
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-09-23 17:40:59 -0500

    Update Persistent.cpp

--- Algoritmos/C++/Segment Tree/Persistent.cpp
+++ Algoritmos/C++/Segment Tree/Persistent.cpp
@@ -1,37 +1,50 @@
-struct Vertex {
-    Vertex *l, *r;
-    int sum;
-
-    Vertex(int val) : l(nullptr), r(nullptr), sum(val) {}
-    Vertex(Vertex *l, Vertex *r) : l(l), r(r), sum(0) {
-        if (l) sum += l->sum;
-        if (r) sum += r->sum;
+struct STP {
+    //segment tree persistent for histograms
+    struct node {
+        int x,l,r;
+        node(int a=0,int b=0, int c=0):x(a),l(b),r(c){}
+    };
+    vector<node>st;
+    vector<int>rt;
+    int N,sz;
+    STP(int size) {
+        N=size,sz=0;
+        push();
+        rt.push_back(0);
     }
+    void push() {
+        sz++;
+        st.push_back(node(0,0,0));
+    }
+    void copy(int u,int v) {
+        st[v]=st[u];
+    }
+    void update(int u, int v, int tl, int tr, int x) {
+        push();
+        copy(u,v);
+        st[v].x++;
+        if(tl==tr)return;
+        int tm=tl+tr>>1;
+        if(x<=tm)
+            update(st[u].l,st[v].l=sz,tl,tm,x);
+        else
+            update(st[u].r,st[v].r=sz,tm+1,tr,x);
+    }
+    int query(int u, int v, int tl, int tr, int l, int r) {
+        if(l>tr||r<tl)
+            return 0;
+        if(l<=tl&&tr<=r)
+            return st[v].x-st[u].x;
+        int tm=tl+tr>>1;
+        return query(st[u].l,st[v].l,tl,tm,l,r)+query(st[u].r,st[v].r,tm+1,tr,l,r);
+    }
+    int query(int u, int v, int l , int r) {
+        return query(rt[u],rt[v],1,N,l,r);
+    }
+    void add(int x) {
+        int lsz=rt.size();
+        rt.push_back(sz);
+        update(rt[lsz-1],rt[lsz],1,N,x);
+    }
+ 
 };
-
-Vertex* build(int a[], int tl, int tr) {
-    if (tl == tr)
-        return new Vertex(a[tl]);
-    int tm = (tl + tr) / 2;
-    return new Vertex(build(a, tl, tm), build(a, tm+1, tr));
-}
-
-int get_sum(Vertex* v, int tl, int tr, int l, int r) {
-    if (l > r)
-        return 0;
-    if (l == tl && tr == r)
-        return v->sum;
-    int tm = (tl + tr) / 2;
-    return get_sum(v->l, tl, tm, l, min(r, tm))
-         + get_sum(v->r, tm+1, tr, max(l, tm+1), r);
-}
-
-Vertex* update(Vertex* v, int tl, int tr, int pos, int new_val) {
-    if (tl == tr)
-        return new Vertex(new_val);
-    int tm = (tl + tr) / 2;
-    if (pos <= tm)
-        return new Vertex(update(v->l, tl, tm, pos, new_val), v->r);
-    else
-        return new Vertex(v->l, update(v->r, tm+1, tr, pos, new_val));
-}
