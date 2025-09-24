bebd5358c7dcaf3a46634d0968c101b279d4ea29
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-09-23 20:24:22 -0500

    Templae

---
+++ Algoritmos/C++/Template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define For(i, k, n) for (int i = k; i < n; i++)
+#define Rfor(i, n, k) for (int i = n; i >= k; i--)
+#define all(v) v.begin(), v.end()
+#define INF(t) numeric_limits<t>::max()
+#define ln "\n"
+#define Pair pair<int,int>
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define RAYA cerr << " ============================ " << endl
