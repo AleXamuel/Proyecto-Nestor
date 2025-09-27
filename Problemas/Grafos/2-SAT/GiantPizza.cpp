ed8b477e39d26304fc48801a4a292afeb9aa815e
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-09-26 21:51:27 -0500

    GiantPizza.cpp

--- Problemas/Grafos/2-SAT/GiantPizza.cpp
+++ Problemas/Grafos/2-SAT/GiantPizza.cpp
@@ -1,105 +1,110 @@
-#include <iostream>
+//https://cses.fi/problemset/task/1684/
+#include <bits/stdc++.h>
 using namespace std;
-#include <vector>
-#include <stack>
-int m;
-vector<vector<int>> adj;
-vector<vector<int>> adjT;
-stack<int> top_sort;
-vector<int> scc;
-vector<bool> state;
-int nscc;
-vector<bool> variables;
 
-void TWOSAT() {
-	adj.resize(m);
-	adjT.resize(m);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define For(i, k, n) for (int i = k; i < n; ++i)
+#define Rfor(i, n, k) for (int i = n; i >= k; --i)
+#define all(v) v.begin(), v.end()
+#define ln '\n'
+
+struct TwoSatSolver {
+    int n_vars;
+    int n_vertices;
+    vector<vector<int> > adj, adj_t;
+    vector<bool> used;
+    vector<int> order, comp;
+    vector<bool> assignment;
+
+    TwoSatSolver(int _n_vars) : n_vars(_n_vars), n_vertices(2 * n_vars), adj(n_vertices), adj_t(n_vertices),
+                                used(n_vertices), order(), comp(n_vertices, -1), assignment(n_vars) {
+        order.reserve(n_vertices);
+    }
+
+    void dfs1(int v) {
+        used[v] = true;
+        for (int u: adj[v]) {
+            if (!used[u])
+                dfs1(u);
+        }
+        order.push_back(v);
+    }
+
+    void dfs2(int v, int cl) {
+        comp[v] = cl;
+        for (int u: adj_t[v]) {
+            if (comp[u] == -1)
+                dfs2(u, cl);
+        }
+    }
+
+    bool solve_2SAT() {
+        order.clear();
+        used.assign(n_vertices, false);
+        for (int i = 0; i < n_vertices; ++i) {
+            if (!used[i])
+                dfs1(i);
+        }
+
+        comp.assign(n_vertices, -1);
+        for (int i = 0, j = 0; i < n_vertices; ++i) {
+            int v = order[n_vertices - i - 1];
+            if (comp[v] == -1)
+                dfs2(v, j++);
+        }
+
+        assignment.assign(n_vars, false);
+        for (int i = 0; i < n_vertices; i += 2) {
+            if (comp[i] == comp[i + 1])
+                return false;
+            assignment[i / 2] = comp[i] > comp[i + 1];
+        }
+        return true;
+    }
+
+    void add_disjunction(int a, bool na, int b, bool nb) {
+        // na and nb signify whether a and b are to be negated
+        a = 2 * a ^ na;
+        b = 2 * b ^ nb;
+        int neg_a = a ^ 1;
+        int neg_b = b ^ 1;
+        adj[neg_a].push_back(b);
+        adj[neg_b].push_back(a);
+        adj_t[b].push_back(neg_a);
+        adj_t[a].push_back(neg_b);
+    }
+};
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    TwoSatSolver solver(m);
+    For(i, 0, n) {
+        char A, B;
+        int a, b;
+        cin >> A >> a >> B >> b;
+        a--, b--;
+        solver.add_disjunction(a, (A == '-'), b, (B == '-'));
+    }
+    if (solver.solve_2SAT()) {
+        For(i, 0, m) {
+            cout << (solver.assignment[i] ? "+" : "-") << " ";
+        }
+        cout << ln;
+    } else {
+        cout << "IMPOSSIBLE" << ln;
+    }
 }
 
-void addEdge(int a, int b) {
-	adj[a].push_back(b);
-	adjT[b].push_back(a);
-}
 
-void dfs(int u) {
-	state[u] = true;
-	for (int v : adj[u])
-		if (!state[v])
-			dfs(v);
-	top_sort.push(u);
-}
-
-void dfs_kos(int u) {
-	scc[u] = nscc;
-	state[u] = true;
-	for (int v : adjT[u])
-		if (!state[v])
-			dfs_kos(v);
-}
-
-void strongly_cc() {
-	state.resize(m,false);
-	for (int i = 0; i < m; i++)
-		if (!state[i])
-			dfs(i);
-	scc.resize(m);
-	nscc = 1;
-	fill(state.begin(), state.end(), false);
-	while (!top_sort.empty()) {
-		int u = top_sort.top();
-		top_sort.pop();
-		if (!state[u]) {
-			dfs_kos(u);
-			nscc++;
-		}
-	}
-}
-
-void asignarVariables() {
-	variables.resize(m, false);
-	for (int i = 0; i < m/2; i++)
-		variables[i] = scc[i] > scc[i + (m / 2)];
-}
-
-
-int main() {
-	int n,o;
-	cin >> n >>o;
-	m = 2 * o;
-	TWOSAT();
-	for (int i = 0; i < n; i++){
-		char signo1,signo2;
-		int num1, num2;
-		cin >> signo1 >>num1>>signo2>>num2;
-		num1--;
-		num2--;
-		if (signo1=='+' && signo2=='+') {
-			addEdge(num1 + o, num2);
-			addEdge(num2 + o, num1);
-		}
-		else if (signo1=='+' && signo2=='-') {
-			addEdge(num1 + o, num2 + o);
-			addEdge(num2, num1);
-		}
-		else if (signo1=='-' && signo2=='+') {
-			addEdge(num1, num2);
-			addEdge(num2 + o, num1 + o);
-		}
-		else {
-			addEdge(num1, num2 + o);
-			addEdge(num2, num1 + o);
-		}
-	}
-	strongly_cc();
-	for (int i = 0; i < o; i++)
-		if (scc[i] == scc[i + o]) {
-			cout<<"IMPOSSIBLE";
-			return 0;
-		}
-	asignarVariables();
-	for (int i = 0; i < o; i++)
-		cout<< (variables[i] ? "+ " : "- ");
-
-	
-	return 0;
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
 }
