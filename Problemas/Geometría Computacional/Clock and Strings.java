479ca64274b290f6edf0186bd5e77c4b0282155f
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-09-28 22:09:18 -0500

    Geometry, Math

--- Problemas/Geometría Computacional/Clock and Strings.java
+++ Problemas/Geometría Computacional/Clock and Strings.java
@@ -2,6 +2,7 @@
 import java.io.*;
 import java.util.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 public class Main {
 
@@ -9,10 +10,10 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter((System.out));
 		StringTokenizer st;
-		HashMap<Integer, Point> H = new HashMap<>();
+		HashMap<Integer, Point2D.Double> H = new HashMap<>();
 		int i = 1;
 		for (int a = 0; a <= 360; a += 30)
-			H.put(i++, new Point(Math.cos(Math.toRadians(a)), Math.sin(Math.toRadians(a))));
+			H.put(i++, new Point2D.Double(Math.cos(Math.toRadians(a)), Math.sin(Math.toRadians(a))));
 		int T = Integer.parseInt(in.readLine());
 		while (T-- > 0) {
 			st = new StringTokenizer(in.readLine());
@@ -20,31 +21,10 @@ public class Main {
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
-			double x1 = H.get(a).x;
-			double y1 = H.get(a).y;
-			double x2 = H.get(b).x;
-			double y2 = H.get(b).y;
-
-			double x3 = H.get(c).x;
-			double y3 = H.get(c).y;
-			double x4 = H.get(d).x;
-			double y4 = H.get(d).y;
-			out.println(Line2D.linesIntersect(x1, y1, x2, y2, x3, y3, x4, y4) ? "YES" : "NO");
+			Line2D l1 = new Line2D.Double(H.get(a), H.get(b));
+			Line2D l2 = new Line2D.Double(H.get(c), H.get(d));
+			out.println(l1.intersectsLine(l2) ? "YES" : "NO");
 		}
 		out.close();
 	}
-
-	static class Point {
-		double x, y;
-
-		Point(double x, double y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		public String toString() {
-			return "(" + x + "," + y + ")";
-		}
-	}
-
 }
