64e3c44467331a2c86ec186b1c877b504e7bfd7c
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-10-04 12:51:46 -0500

    Normal Query

--- Algoritmos/C++/Segment Tree/Normal Query.cpp
+++ Algoritmos/C++/Segment Tree/Normal Query.cpp
@@ -1,11 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class ST {
 public:
     vector<int> t;
     int n;
+
     int combine(int a, int b) {
-        return (a+b);
+        return (a + b);
     }
-    void buildST(vector<int>& a, int v, int tl, int tr) {
+
+    void buildST(vector<int> &a, int v, int tl, int tr) {
         if (tl == tr) {
             t[v] = a[tl];
         } else {
@@ -15,18 +20,20 @@ public:
             t[v] = combine(t[v * 2], t[v * 2 + 1]);
         }
     }
-    int sumST(int v, int tl, int tr, int l, int r) {
+
+    int query(int v, int tl, int tr, int l, int r) {
         if (l > r)
-            return -2e9;
+            return 0;
         if (l == tl && r == tr)
             return t[v];
         int tm = (tl + tr) / 2;
-        return combine(sumST(v * 2, tl, tm, l, min(r, tm)),
-                   sumST(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
+        return combine(query(v * 2, tl, tm, l, min(r, tm)),
+                       query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
     }
+
     void updateST(int v, int tl, int tr, int pos, int new_val) {
         if (tl == tr) {
-            t[v] =new_val;
+            t[v] = new_val;
         } else {
             int tm = (tl + tr) / 2;
             if (pos <= tm)
@@ -36,18 +43,22 @@ public:
             t[v] = combine(t[v * 2], t[v * 2 + 1]);
         }
     }
-    ST(vector<int>& a) {
+
+    ST(vector<int> &a) {
         n = a.size();
         t.resize(4 * n);
         buildST(a, 1, 0, n - 1);
     }
+
     ST(int sz) {
-        n=sz;
-        t.assign(4*n,0);
+        n = sz;
+        t.assign(4 * n, 0);
     }
+
     int query(int l, int r) {
-        return sumST(1, 0, n - 1, l, r);
+        return query(1, 0, n - 1, l, r);
     }
+
     void update(int pos, int x) {
         updateST(1, 0, n - 1, pos, x);
     }
