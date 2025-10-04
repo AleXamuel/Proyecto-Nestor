cb805d7baeb1db845c87d4c91bbdf260b018ba32
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-10-04 13:21:45 -0500

    Template

--- Algoritmos/C++/Template.cpp
+++ Algoritmos/C++/Template.cpp
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ull unsigned long long
+#define ull unsigned ll
 #define For(i, k, n) for (int i = k; i < n; i++)
 #define Rfor(i, n, k) for (int i = n; i >= k; i--)
-#define all(v) v.begin(), v.end()
+#define all(v) ((v).begin(), (v).end())
 #define INF(t) numeric_limits<t>::max()
 #define ln "\n"
+#define sz(v) (int)((v).size())
 #define Pair pair<int,int>
 #define DBG(x) cerr << #x << " = " << (x) << endl
 #define RAYA cerr << " ============================ " << endl
 
 template<typename T>
 ostream &operator<<(ostream &os, vector<T> A) {
+    if (A.empty()) {
+        os << "[]";
+        return os;
+    }
     os << "[";
-    For(i, 0, A.size()-1)
+    For(i, 0, sz(A)-1)
         os << A[i] << ", ";
     os << A.back() << "]";
     return os;
