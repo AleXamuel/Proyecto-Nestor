4e29824e29cf7614c7b2f9031a0f68ff5eb27d9a
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-10-08 16:52:35 -0500

    Pigeon Principle

---
+++ Problemas/Maths/Problems/Batteries.cpp
@@ -0,0 +1,72 @@
+//https://codeforces.com/contest/2155/problem/D
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned ll
+#define For(i, a, b) for (int i = a; i < b; i++)
+#define Rfor(i, n, k) for (int i = n; i >= k; i--)
+#define all(v) (v).begin(), (v).end()
+#define INF(t) numeric_limits<t>::max()
+#define ln "\n"
+#define sz(v) (int)((v).size())
+#define Pair pair<ll,ll>
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define RAYA cerr << " ============================ " << endl
+
+
+ostream &operator<<(ostream &os,Pair A) {
+    os << "(" << A.first << "," << A.second << ")";
+    return os;
+}
+
+template<typename T>
+ostream &operator<<(ostream &os, vector<T> A) {
+    if (A.empty()) {
+        os << "[]";
+        return os;
+    }
+    os << "[";
+    For(i, 0, sz(A)-1)
+        os << A[i] << ", ";
+    os << A.back() << "]";
+    return os;
+}
+
+int mod(int a, int b) {
+    if (b < 0)
+        b *= -1;
+    return (a % b + b) % b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> A;
+    For(i, 1, n+1)
+        A.push_back(i);
+    while (true) {
+        For(k, 1, n+1) {
+            For(i, 0, n) {
+                int a = A[i];
+                int b = A[(i + k) % n];
+                cout << a << " " << b << ln;
+                int x;
+                cin >> x;
+                if (x == 1) {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
+int main() {
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
