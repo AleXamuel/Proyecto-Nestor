2eb02dfd1f73ba48ca5f255ccc45f7fecb181a20
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-10-08 11:25:54 -0500

    Update CP Normal Query.cpp

--- Algoritmos/C++/Segment Tree/CP Normal Query.cpp
+++ Algoritmos/C++/Segment Tree/CP Normal Query.cpp
@@ -1,4 +1,4 @@
-int n, t[4*nmax],A[nmax];
+int n, t[4*MAX],A[MAX];
 int combine(int a , int b) {
     return a+b;
 }
@@ -12,15 +12,15 @@ void build( int v, int tl, int tr) {
         t[v] = combine(t[v*2] , t[v*2+1]);
     }
 }
-int sum(int v, int tl, int tr, int l, int r) {
+int query(int v, int tl, int tr, int l, int r) {
     if (l > r)
         return 0;
     if (l == tl && r == tr) {
         return t[v];
     }
     int tm = (tl + tr) / 2;
-    return combine(sum(v*2, tl, tm, l, min(r, tm))
-           , sum(v*2+1, tm+1, tr, max(l, tm+1), r));
+    return combine(query(v*2, tl, tm, l, min(r, tm))
+           , query(v*2+1, tm+1, tr, max(l, tm+1), r));
 }
 void update(int v, int tl, int tr, int pos, int new_val) {
     if (tl == tr) {
