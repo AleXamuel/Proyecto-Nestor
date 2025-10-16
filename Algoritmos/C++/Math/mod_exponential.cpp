2b52df0edb0d1001594390fd361ebd8e621b156d
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-10-16 12:52:04 -0500

    Delete Algoritmos/C++/Math/mod_exponential.cpp

--- Algoritmos/C++/Math/mod_exponential.cpp
+++
@@ -1,9 +0,0 @@
-ll modExp(ll base, ll exp, ll mod) {
-    ll res = 1,x=base%mod;
-    while (exp>0) {
-        if ((exp & 1)>0) res=res*x%mod;
-        x = x*x%mod;
-        exp >>= 1;
-    }
-    return res;
-}
