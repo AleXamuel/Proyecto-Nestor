2e121b254991d4c3c1a19144fbc3b6623c276080
Gabriel Gómez <119649109+GabrielGomezCal@users.noreply.github.com>
2025-10-28 15:39:04 -0500

    Gauss-Jordan elimination
    Implement Gauss-Jordan elimination algorithm for solving linear equations.

---
+++ Algoritmos/C++/Math/Gauss Jordan.cpp
@@ -0,0 +1,49 @@
+const double EPS = 1e-9;
+const int INF = 2;
+
+int gauss(vector<vector<double> > a, vector<double> &ans) {
+    int n = (int) a.size();
+    int m = (int) a[0].size() - 1;
+
+    vector<int> where(m, -1);
+    for (int col = 0, row = 0; col < m && row < n; ++col) {
+        int sel = row;
+        for (int i = row; i < n; ++i)
+            if (abs(a[i][col]) > abs(a[sel][col]))
+                sel = i;
+        if (abs(a[sel][col]) < EPS)
+            continue;
+        for (int i = col; i <= m; ++i)
+            swap(a[sel][i], a[row][i]);
+        where[col] = row;
+
+        for (int i = 0; i < n; ++i)
+            if (i != row) {
+                double c = a[i][col] / a[row][col];
+                for (int j = col; j <= m; ++j)
+                    a[i][j] -= a[row][j] * c;
+            }
+        ++row;
+    }
+
+    ans.assign(m, 0);
+    for (int i = 0; i < m; ++i)
+        if (where[i] != -1)
+            ans[i] = a[where[i]][m] / a[where[i]][i];
+    for (int i = 0; i < n; ++i) {
+        double sum = 0;
+        for (int j = 0; j < m; ++j)
+            sum += ans[j] * a[i][j];
+        if (abs(sum - a[i][m]) > EPS)
+            return 0;
+    }
+
+    for (int i = 0; i < m; ++i)
+        if (where[i] == -1)
+            return INF;
+    return 1;
+}
+
+//se crea la matriz y se pasa con un vector<double> vacio ans
+//vector<vector<double> > mat(3, vector<double>(4)); para este caso de 3 filas 4 columnas
+//tendremos como respuesta la cantidad de (columnas - 1)
