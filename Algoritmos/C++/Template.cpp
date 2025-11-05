90a07ac8f2a3cf9a0e779ab8d69bce16da769356
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-11-05 12:57:19 -0500

    Template

--- Algoritmos/C++/Template.cpp
+++ Algoritmos/C++/Template.cpp
@@ -2,25 +2,33 @@
 using namespace std;
 #define ll long long
 #define ull unsigned ll
-#define For(i, k, n) for (int i = k; i < n; i++)
+#define For(i, a, b) for (int i = a; i < b; i++)
 #define Rfor(i, n, k) for (int i = n; i >= k; i--)
-#define all(v) ((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
 #define INF(t) numeric_limits<t>::max()
 #define ln "\n"
 #define sz(v) (int)((v).size())
 #define Pair pair<int,int>
+#define MOD(a,b) ((a%abs(b)+abs(b))%abs(b))
 #define DBG(x) cerr << #x << " = " << (x) << endl
 #define RAYA cerr << " ============================ " << endl
+#define DBGA(A) cerr << #A << " = "; for (auto &i : A) cerr << i << " "; cerr << endl;
+#define DBGP(P) cerr<<#P<<" =("<<P.first<<","<<P.second<<")"<<endl;
 
-template<typename T>
-ostream &operator<<(ostream &os, vector<T> A) {
-    if (A.empty()) {
-        os << "[]";
-        return os;
-    }
-    os << "[";
-    For(i, 0, sz(A)-1)
-        os << A[i] << ", ";
-    os << A.back() << "]";
-    return os;
+
+void solve() {
+
 }
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
+
+
+
+
