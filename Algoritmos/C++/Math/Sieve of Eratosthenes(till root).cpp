bc54f1b0770e6faf603f5a1ed3ac9a5fefb2aa45
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2025-11-15 13:12:56 -0500

    Optimization of the Sieve of Eratosthenes

---
+++ Algoritmos/C++/Math/Sieve of Eratosthenes(till root).cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned ll
+#define For(i, a, b) for (int i = a; i < b; i++)
+#define Rfor(i, n, k) for (int i = n; i >= k; i--)
+#define all(v) (v).begin(), (v).end()
+#define INF(t) numeric_limits<t>::max()
+#define ln "\n"
+#define sz(v) (int)((v).size())
+
+vector<bool> is_prime;
+
+void sieve(int n) {
+    is_prime.assign(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; i++)
+        if (is_prime[i])
+            for (int j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+}
