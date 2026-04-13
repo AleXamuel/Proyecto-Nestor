d705ede39c595e2254f7dcde23ea3e3b4dd79396
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2026-04-12 20:11:57 -0500

    Update BellmandFord.cpp

--- Algoritmos/C++/Grafos/BellmandFord.cpp
+++ Algoritmos/C++/Grafos/BellmandFord.cpp
@@ -1,3 +1,25 @@
+//normal
+struct Edge {
+    int a, b, cost;
+};
+
+int n, m, v;
+vector<Edge> edges;
+const int INF = 1000000000;
+
+void solve()
+{
+    vector<int> d(n, INF);
+    d[v] = 0;
+    for (int i = 0; i < n - 1; ++i)
+        for (Edge e : edges)
+            if (d[e.a] < INF)
+                d[e.b] = min(d[e.b], d[e.a] + e.cost);
+    // display d, for example, on the screen
+}
+
+
+//for negative cycles
 struct Edge {
     int a, b, cost;
 };
