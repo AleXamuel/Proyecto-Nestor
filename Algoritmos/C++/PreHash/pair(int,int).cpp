4075530199a704c70260d755f38b7605eda9e7a9
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2026-05-12 12:59:07 -0500

    Hash Function

--- Algoritmos/C++/PreHash/pair(int,int).cpp
+++ Algoritmos/C++/PreHash/pair(int,int).cpp
@@ -6,3 +6,13 @@ struct PairHash {
         return hash1 ^ hash2*31;
     }
 };
+
+//Stronger hash function:
+struct PairHash {
+    template<typename T1, typename T2>
+    size_t operator()(const pair<T1, T2>& p) const {
+        size_t h1 = hash<T1>{}(p.first);
+        size_t h2 = hash<T2>{}(p.second);
+        return h1 ^ (h2 * 2654435761ULL + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
+    }
+};
