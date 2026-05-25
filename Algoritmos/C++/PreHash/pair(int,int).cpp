0d4f77e625f515f07207025c5ea08bcf11a929c1
Ian Montaño Pardo <126417705+IanPardo02@users.noreply.github.com>
2026-05-25 15:10:08 -0500

    CustomHash

--- Algoritmos/C++/PreHash/pair(int,int).cpp
+++ Algoritmos/C++/PreHash/pair(int,int).cpp
@@ -16,3 +16,18 @@ struct PairHash {
         return h1 ^ (h2 * 2654435761ULL + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
     }
 };
+
+struct custom_hash {
+    static ull splitmix64(ull x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(ull x) const {
+        static const ull FIXED_RANDOM =
+                chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
