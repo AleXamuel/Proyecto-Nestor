e236db78ba3c04c5d00152bd339d1d2a77861c25
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2026-06-09 15:11:50 -0500

    Update RMQ.cpp

--- Algoritmos/C++/Sparse table/RMQ.cpp
+++ Algoritmos/C++/Sparse table/RMQ.cpp
@@ -1,12 +1,35 @@
-int st[20][MAX],lg[MAX+1];;
-int getMin(int L , int R) {
-    int i = lg[R - L + 1];
-    return min(st[i][L], st[i][R - (1 << i) + 1]);
-}
-lg[1] = 0;
-for (int i = 2; i <= MAX; i++)
-    lg[i] = lg[i/2] + 1;
-copy(array.begin(), array.end(), st[0]);
-for (int i = 1; i <= 19; i++)
-    for (int j = 0; j + (1 << i) <= s.size(); j++)
-        st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+struct ST {
+    int n, K;
+    vector<vector<int>> st;
+    vector<int> lg;
+ 
+    ST(const vector<int>& a) {
+        n = a.size();
+        lg.resize(n + 1);
+        lg[1] = 0;
+        for (int i = 2; i <= n; i++)
+            lg[i] = lg[i / 2] + 1;
+ 
+        K = lg[n] + 1;
+        st.assign(K, vector<int>(n));
+        for (int i = 0; i < n; i++)
+            st[0][i] = a[i];
+        for (int k = 1; k < K; k++) {
+            for (int i = 0; i + (1 << k) <= n; i++) {
+                    st[k][i] = max(
+                    st[k - 1][i],
+                    st[k - 1][i + (1 << (k - 1))]
+                );
+            }
+        }
+    }
+    int query(int l, int r) {
+        if(l>r)
+            return 0;
+        int k = lg[r - l + 1];
+        return max(
+            st[k][l],
+            st[k][r - (1 << k) + 1]
+        );
+    }
+};
